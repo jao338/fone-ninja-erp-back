975b49f4f32dcf58f3b35294a956065d9acf0bc9
João Henrique dos Santos Pereira <joaohenriquerc123@gmail.com>
2025-12-14 21:38:44 -0300

    feat: Adicionando filtros à listagem de produtos e implementando show

--- domain/Models/Product/Actions/IndexAction.php
+++ domain/Models/Product/Actions/IndexAction.php
@@ -12,13 +12,24 @@ final readonly class IndexAction {
     public function handle(array $data): LengthAwarePaginator
     {
 
-        //  Adicionar filtros
+        $name               = $data['nome'] ?? null;
+        $average_cost       = $data['custo_medio'] ?? null;
+        $sale_price         = $data['preco_venda'] ?? null;
+        $amount             = $data['quantidade'] ?? null;
+        $created_at         = $data['criado_em'] ?? null;
+        $updated_at         = $data['atualizado_em'] ?? null;
 
         $tipo_ordenacao     = $data['tipo_ordenacao'] ?? null;
         $campo_ordenacao    = $data['campo_ordenacao'] ?? null;
         $por_pagina         = $data['por_pagina'] ?? 20;
 
         return $this->model
+            ->when(isset($name), fn($query)         => $query->where('name', 'LIKE', "%$name%"))
+            ->when(isset($average_cost), fn($query) => $query->where('average_cost', $average_cost))
+            ->when(isset($sale_price), fn($query)   => $query->where('sale_price', $sale_price))
+            ->when(isset($amount), fn($query)       => $query->where('amount', $amount))
+            ->when(isset($created_at), fn($query)   => $query->where('created_at', $created_at))
+            ->when(isset($updated_at), fn($query)   => $query->where('updated_at', $updated_at))
             ->when(
                 !isset($campo_ordenacao) && !isset($tipo_ordenacao),
                 fn($query) => $query->orderBy('criado_em', 'DESC'),
